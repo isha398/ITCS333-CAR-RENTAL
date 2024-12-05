f784f2bc33430d6a66e7c6d0af6d4863903858e2
isha398 <154532046+isha398@users.noreply.github.com>
2024-12-05 21:29:50 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once dirname(__DIR__) . '/config/config.php';
+
+if(session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+session_unset();
+session_destroy();
+
+header('Location: ' . BASE_URL . '/login');
+exit();
+
+?>
