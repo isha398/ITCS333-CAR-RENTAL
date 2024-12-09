202dc6dfbc18bae4e5f3e27dc06c406428f26925
isha398 <154532046+isha398@users.noreply.github.com>
2024-12-09 10:59:50 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,73 +1,18 @@
-<?php 
-include_once dirname(__DIR__) . '/config/config.php'; 
-// Start Session
+
+<?php
+
+include_once dirname(__DIR__) . '/config/config.php';
 
 if(session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
+session_unset();
+session_destroy();
+
+header('Location: ' . BASE_URL . '/login');
+exit();
+
 
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/main.css">
-    <title>Car Booking | Signup</title>
-</head>
- 
-<body>
-    <?php include_once '../components/navbar.php'; ?>
-    <section class="signup-container">
-
-
-        
-        <div class="signup-box">
-            <h2>Sign Up</h2>
-            <?php 
-                if(isset($_SESSION['error'])) {
-                    echo "<div class='error-message'>" . $_SESSION['error'] . "</div>";
-                    unset($_SESSION['error']);
-                }
-                
-                if(isset($_SESSION['success'])) {
-                    echo "<div class='success-message'>" . $_SESSION['success'] . "</div>";
-                    unset($_SESSION['success']);
-                }
-            ?>
-            <form class="signup-form" action="process_signup.php" method="POST">
-                <div class="form-group">
-                    <label for="fullname">Full Name</label>
-                    <input type="text" id="fullname" name="fullname" required>
-                </div>
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" id="email" name="email" required>
-                </div>
-                <div>
-                    <div class="form-group">
-                        <label for="username">Username</label>
-                        <input type="text" id="username" name="username" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" id="password" name="password" required>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="repassword">Confirm Password</label>
-                    <input type="password" id="repassword" name="repassword" required>
-                </div>
-                <button type="submit" class="signup-btn">Sign Up</button>
-                <p class="signup-text">
-                    Already have an account? <a href="<?php echo BASE_URL ?>/login">Log In</a>
-                </p>
-            </form>
-        </div>
-    </section>
-</body>
-
-</html>
